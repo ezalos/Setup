32c6a348f86c2ac3bf840b085f0b550800addc78
Louis DEVELLE <ldevelle@e3r7p12.42.fr>
2020-10-07 16:34:47 +0200

    python

--- oh-my-zsh-themes.sh
+++ oh-my-zsh-themes.sh
@@ -2,6 +2,14 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	# Linux
 	sed -i 's~ZSH_THEME=\"robbyrussell~ZSH_THEME=\"agnoster~g' ~/.zshrc
+	# clone
+	git clone https://github.com/powerline/fonts.git --depth=1
+	# install
+	cd fonts
+	./install.sh
+	# clean-up a bit
+	cd ..
+	rm -rf fonts
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	# Mac OSX
 	sed -i '' 's~ZSH_THEME=\"robbyrussell~ZSH_THEME=\"agnoster~g' ~/.zshrc
